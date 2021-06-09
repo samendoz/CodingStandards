cbc3cad4adacaf3751b589fd37a36f9eaa75528d
Sebastian Mendoza <samendoz@espol.edu.ec>
2021-06-09 18:25:35 -0500

    taller completado

--- src/main/java/labcodingstandards/Calculator.java
+++ src/main/java/labcodingstandards/Calculator.java
@@ -1,24 +1,26 @@
-//
-//
-
+// Copyright (C) 2020
+// All rights reserved
 package labcodingstandards;
 
 import java.util.Scanner;
 
 
 /**
- * @author
+ * @author smend
  *
  */
 public class Calculator {	
-	
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
 		
         System.out.print("1. +\n2. -\n3. *\n4. /\nEnter an operator: ");
         
         char operator = reader.nextLine().charAt(0);
-        double First;
+        double first;
         double second;
         String input;
         
@@ -27,7 +29,7 @@ public class Calculator {
         	input = reader.nextLine();
         	
             try {
-            	First=Integer.parseInt(input);
+            	first=Integer.parseInt(input);
                 break;
             } catch (NumberFormatException e) {
                 System.out.println("Not valid!");
@@ -47,13 +49,19 @@ public class Calculator {
         }
 
         Calculator cal=new Calculator();
-        String result=cal.Operation(First,second,operator);
+        String result=cal.operation(first, second, operator);
 
         System.out.printf(result);
 		reader.close();
 	}
-
-	private String Operation(double first,double second,char operator) {
+	/**
+	 * 
+	 * @param first
+	 * @param second
+	 * @param operator
+	 * @return String
+	 */
+	private String operation(double first, double second, char operator) {
 		double result = 0;
 		switch(operator)
         {
